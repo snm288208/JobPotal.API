79fb4fa8405ab28632147e8c53ef6ce83c64faec
snm288208 <96367383+snm288208@users.noreply.github.com>
2023-07-15 10:56:12 +0000

    sas

--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,5 @@
 
-from typing import List, Optional
+from typing import List
 from fastapi import APIRouter
 from db.models.user import User
 from db.schema.userschema import UserResponse, user_list
@@ -12,9 +12,9 @@ from fastapi.responses import JSONResponse
 userrouter = APIRouter()
 
 @userrouter.get("/home")
-def getuser(userName: str):
+def getuser(userName: str, response_model = ApiResponse[List[UserResponse]]):
      user = user_list(User_collection.find({"userName":userName}))
-     response = ApiResponse[list[UserResponse]](Result= user, StatsCode='200', Message= "QueriedSucessFully")
+     response = ApiResponse[List[UserResponse]](Result= user, StatsCode='200', Message= "QueriedSucessFully")
      print (response)
      return response
 
